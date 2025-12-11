2837e8c3490895e8b2897ba1f49c99ae8608dde2
Victor Monroy <vmmonroyv@gmail.com>
2025-12-11 12:10:39 -0600

    feature: custom user dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+
+                    <h1 class="text-3xl font-bold">
+                        Bienvenido {{ Auth::user()->name }}
+                    </h1>
+
+                    <p class="mt-2 text-gray-700 dark:text-gray-300">
+                        Este es tu dashboard personalizado.
+                    </p>
+
                 </div>
             </div>
         </div>
